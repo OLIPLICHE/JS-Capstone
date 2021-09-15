5cd4406f657e3947bdf67292110638723a3360aa
olipliche <olipliche@gmail.com>
2021-09-15 20:48:52 +0100

    Add display list of item on the homePage

---
+++ src/modules/data.js
@@ -0,0 +1,17 @@
+/* eslint-disable indent */
+/* eslint-disable no-console */
+/* eslint-disable linebreak-style */
+const API = {
+  getMeals: async (endpoint) => {
+    console.log(endpoint);
+    const res = await fetch(`${endpoint}`, { method: 'GET' });
+    if (!res.ok) {
+      throw new Error(`API error! status: ${res.status}`);
+    } else {
+      const data = await res.json();
+      return data;
+    }
+  },
+};
+
+export default API;
