f1f12f8653219b59c931ba96b6a0734bb5a44034
olipliche <olipliche@gmail.com>
2021-09-16 16:31:33 +0100

    Update  display like

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import './style.css';
 import API from './modules/data';
 import renderMeals from './modules/app';
 
-const endpoint = 'https://www.themealdb.com/api/json/v1/1/';
+const endpoint = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood';
 const likesEndpoint = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/lvQFdeSbvijlsDX1Y4VY/likes/';
 const displayMeals = () => {
   Promise.all([
-    API.getMeals(`${endpoint}search.php?f=b`),
+    API.getMeals(`${endpoint}`),
     API.getLikes(likesEndpoint),
   ]).then((data) => {
     renderMeals(data[0], data[1]);
