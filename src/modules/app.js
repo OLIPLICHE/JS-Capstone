f0400833b6200bf3382a81a5d09780f5c73c0534
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-09-16 12:43:16 +0300

    Create popup

--- src/modules/app.js
+++ src/modules/app.js
@@ -34,6 +34,14 @@ const renderMeals = (mealsData) => {
     likeBtn.appendChild(likeIcon);
 
     const commentBtn = document.createElement('button');
+    commentBtn.addEventListener('click', () => {
+      const div = document.createElement('div');
+      div.className = 'modal';
+      const divInner = document.createElement('div');
+      divInner.className = 'modal-content'
+      ul.appendChild(div)
+      div.appendChild(divInner)
+    })
     commentBtn.className = 'btn';
     commentBtn.innerText = 'Comment';
     li.appendChild(commentBtn);
